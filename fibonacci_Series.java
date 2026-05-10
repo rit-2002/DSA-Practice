35209bf3a9da5b8244d5b8dac0b52c78d2ce3c26
rit-2002 <128469291+rit-2002@users.noreply.github.com>
2026-05-10 15:49:34 +0530

    Create fibonacci_Series.java

---
+++ fibonacci_Series.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fibonacci_Series{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("ENter Number of terms: ");
+        int n=sc.nextInt();
+        int first=0, second=1;
+        System.out.println("Fibonacci Series: ");
+        for (int i=1;i<=n;i++){
+            System.out.println(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        sc.close();
+    }
+}
