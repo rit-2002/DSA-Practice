9b8e1bebdbfcbb87be1ff28922e95b971e3eff02
rit-2002 <128469291+rit-2002@users.noreply.github.com>
2026-05-18 11:58:52 +0530

    Create solidSquarePattern.java

---
+++ solidSquarePattern.java
@@ -0,0 +1,13 @@
+class solidSquarePattern{
+public static void main(String[] args) {
+    
+    int n=4;
+
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+}
